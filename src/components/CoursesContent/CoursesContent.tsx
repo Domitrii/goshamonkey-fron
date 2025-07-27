4a8b859835ba7fe44a6907c3549394267e16a3c7
Domitrii <126395589+Domitrii@users.noreply.github.com>
2025-07-27 18:13:41 +0200

    video and responsive

--- src/components/CoursesContent/CoursesContent.tsx
+++ src/components/CoursesContent/CoursesContent.tsx
@@ -5,7 +5,7 @@ function CoursesContent() {
     <div className={css.mainCont}>
         <div className={css.firstCont}>
             <p className={css.blackBack} />
-            <video autoPlay muted loop className={css.firstVid}>
+            <video autoPlay muted loop disablePictureInPicture className={css.firstVid}>
                 <source src="/Сайт1.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
