464d7d788ee37de635128ab772616831e4229068
Domitrii <126395589+Domitrii@users.noreply.github.com>
2025-07-31 00:28:58 +0200

    vercel file and background img

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -4,9 +4,9 @@ import AllSection from '../AllSection/AllSection'
 
 
 const products = [
-    {img: "../../../goshaPremium.png",text: "Премиальный курс Гоши",price: " $350.00",index: "courses"},
-    {img: "../../../gosha.png",text: "Курс Гоши",price: "$55.00",index: "courses"},
-    {img: "../../../course1.png",text: "Привет от Гоши",price: "100₽",index: "hi-price"},
+    {img: "../../../goshaPremium.png",text: "Премиальный курс от Гоши-Бомжезьяна",price: " $350.00",index: "courses"},
+    {img: "../../../gosha.png",text: "Персональный курс от Гоши-Бомжезьяна",price: "$55.00",index: "courses"},
+    {img: "../../../course1.png",text: "Персональный привет от Гоши-Бомжезьяна",price: "100₽",index: "hi-price"},
     {img: "../../../course1.png",text: "Привет от Гоши и Льохи",price: "100₽",index: "hi-price"},
 ]
 
